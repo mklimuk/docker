701cb163f0476e781ce695263884932dbdc81021
Michał Klimuk <michal.klimuk@gmail.com>
2017-09-05 14:00:30 +0200

    Updated with webkit2gtk dependencies

--- build/golang-arm/build-arm.sh
+++
@@ -1,9 +0,0 @@
-echo "Bootstrapping linux/arm-7..."
-CC=arm-linux-gnueabihf-gcc-4.9 GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CGO_CFLAGS="-march=armv7-a" CGO_CXXFLAGS="-march=armv7-a" go install std
-
-echo "Compiling for linux/arm-7..."
-export PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig
-CC=arm-linux-gnueabihf-gcc-4.9 CXX=arm-linux-gnueabihf-g++-4.9 GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 CGO_CFLAGS="-march=armv7-a -fPIC" CGO_CXXFLAGS="-march=armv7-a -fPIC" go build -v -x --ldflags="-v" -o "$1" $2
-
-echo "Cleaning up Go runtime for linux/arm-7..."
-rm -rf /usr/local/go/pkg/linux_arm
